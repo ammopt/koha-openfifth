c58d5211048e2545eddf699c7275720099263d03
Jonathan Druart <jonathan.druart@bugs.koha-community.org>
2025-04-08 13:03:59 +0200

    [wip] Move preservation

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/ResourceList.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/ResourceList.vue
@@ -51,7 +51,6 @@ export default {
         goToResourceShow: Function,
         goToResourceEdit: Function,
         doResourceDelete: Function,
-        goToResourceAdd: Function,
         doResourceSelect: Function,
         getResourceShowURL: Function,
         resourceName: String,
